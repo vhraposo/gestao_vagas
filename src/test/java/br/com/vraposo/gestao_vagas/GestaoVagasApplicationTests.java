b0903b56d54c0de1b0c6865a29766ec921146919
vhraposo <thevhup@gmail.com>
2023-11-10 10:32:10 -0300

    create maven project

---
+++ src/test/java/br/com/vraposo/gestao_vagas/GestaoVagasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vraposo.gestao_vagas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoVagasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
