8249d7d9eda8bb96385cb0e2dbefd7fef4720b9b
vhraposo <thevhup@gmail.com>
2023-11-13 11:27:49 -0300

    feat: add exception handling and validation to CandidateEntity and CandidateController

--- src/main/java/br/com/vraposo/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/vraposo/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -2,6 +2,11 @@ package br.com.vraposo.gestao_vagas.modules.candidate;
 
 import java.util.UUID;
 
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
@@ -9,8 +14,15 @@ public class CandidateEntity {
     
     private UUID id;
     private String name;
+
+    @NotBlank
+    @Pattern(regexp = "\\S+", message = "O campo [username] não deve conter espaço.")
     private String username;
+
+    @Email(message = "O campo [email] deve ser um email válido")
     private String email;
+   
+    @Length(min = 10, max = 100, message = "O campo [password] deve ter entre 10 e 100 caracteres.")
     private String password;
     private String description;
     private String curriculum;
