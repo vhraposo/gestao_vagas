adf1031b650023653442abe8906350570df2d622
vhraposo <thevhup@gmail.com>
2023-11-13 22:52:03 -0300

    feat: add password encoder and permitAll for candidate and company endpoints in SecurityConfig.java

--- src/main/java/br/com/vraposo/gestao_vagas/security/SecurityConfig.java
+++ src/main/java/br/com/vraposo/gestao_vagas/security/SecurityConfig.java
@@ -3,6 +3,8 @@ package br.com.vraposo.gestao_vagas.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,11 +16,13 @@ public class SecurityConfig {
         .authorizeHttpRequests(auth -> {
             auth.requestMatchers("/candidate/").permitAll()
                  .requestMatchers("/company/").permitAll();
-            auth.anyRequest().authenticated();
-            
+            auth.anyRequest().authenticated();          
         })
-       
        ;
        return http.build();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
