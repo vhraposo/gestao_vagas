06fbe110a416cf3e680e5c516d8b85481fa324bc
vhraposo <thevhup@gmail.com>
2023-11-14 12:12:53 -0300

    feat: add JWTProvider and SecurityFilter classes for token validation

--- src/main/java/br/com/vraposo/gestao_vagas/security/SecurityFilter.java
+++ src/main/java/br/com/vraposo/gestao_vagas/security/SecurityFilter.java
@@ -1,10 +1,15 @@
 package br.com.vraposo.gestao_vagas.security;
 
 import java.io.IOException;
+import java.util.Collections;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import br.com.vraposo.gestao_vagas.providers.JWTProvider;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,14 +18,28 @@ import jakarta.servlet.http.HttpServletResponse;
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
 
+    @Autowired
+    private JWTProvider jwtProvider;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-        
-        String header = request.getHeader("Authorization");
+                 SecurityContextHolder.getContext().setAuthentication(null);
 
-        System.out.println(header);
-        
+                String header = request.getHeader("Authorization");
+
+            if(header != null){
+            var subjectToken = this.jwtProvider.validateToken(header);
+            if(subjectToken.isEmpty()){
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                return;
+            }
+            request.setAttribute("company_id", subjectToken);
+            UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(subjectToken, null, Collections.emptyList());
+            
+            SecurityContextHolder.getContext().setAuthentication(auth);
+        }
+
         filterChain.doFilter(request, response);
     }
     
