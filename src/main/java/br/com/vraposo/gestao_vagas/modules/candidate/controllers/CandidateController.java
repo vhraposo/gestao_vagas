72de918c36a9b4713dd80985378435a04e1c93d3
vhraposo <thevhup@gmail.com>
2023-11-16 23:11:19 -0300

    Refactor: add Swagger annotations to CandidateEntity and CandidateController

--- src/main/java/br/com/vraposo/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/vraposo/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -32,6 +32,7 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/candidate")
+@Tag(name = "Candidato", description = "Informações do candidato")
 public class CandidateController {
 
     @Autowired
@@ -44,6 +45,13 @@ public class CandidateController {
     private ListAllJobsByFilterUseCase listAllJobsByFilterUseCase;
         
     @PostMapping("/")
+    @Operation(summary = "Cadastro de candidato", description= "Essa função é responsável por cadastrar um cantidato.")
+     @ApiResponses({
+        @ApiResponse(responseCode = "200", content = {
+            @Content(schema = @Schema(implementation = CandidateEntity.class))
+        }),
+        @ApiResponse(responseCode = "400", description = "Usuário já existe")
+    })
     public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity createCandidateRequest) {
         try {
                 var result = this.createCandidateUseCase.execute(createCandidateRequest);
@@ -55,7 +63,6 @@ public class CandidateController {
 
     @GetMapping("/")
     @PreAuthorize("hasRole('CANDIDATE')")
-    @Tag(name = "Candidato", description = "Informações do candidato")
     @Operation(summary = "Perfil do candidato", description= "Essa função é responsável por buscar as informações do perfil do candidato.")
     @ApiResponses({
         @ApiResponse(responseCode = "200", content = {
@@ -78,7 +85,6 @@ public class CandidateController {
 
     @GetMapping("/job")
     @PreAuthorize("hasRole('CANDIDATE')")
-    @Tag(name = "Candidato", description = "Informações do candidato")
     @Operation(summary = "Listagem de vagas disponíveis para o candidato", description= "Essa função é responsável por listar todas as vagas disponíveis, baseada no filtro.")
     @ApiResponses({
         @ApiResponse(responseCode = "200", content = {
