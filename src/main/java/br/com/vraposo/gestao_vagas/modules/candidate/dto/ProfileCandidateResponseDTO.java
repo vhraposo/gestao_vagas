7fda5eaf0f3597c8ce42f6df8f93f8d9b8f15655
vhraposo <thevhup@gmail.com>
2023-11-16 22:43:46 -0300

    feat: add profile endpoint to CandidateController

--- src/main/java/br/com/vraposo/gestao_vagas/modules/candidate/dto/ProfileCandidateResponseDTO.java
+++ src/main/java/br/com/vraposo/gestao_vagas/modules/candidate/dto/ProfileCandidateResponseDTO.java
@@ -2,6 +2,7 @@ package br.com.vraposo.gestao_vagas.modules.candidate.dto;
 
 import java.util.UUID;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,9 +14,16 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class ProfileCandidateResponseDTO {
     
+    @Schema(example = "Desenvolvedora Java")
     private String description;
+
+    @Schema(example = "msilva")
     private String username;
+
+    @Schema(example = "maria@gmail.cin")
     private String email;
     private UUID id;
+
+    @Schema(example = "Maria Silva")
     private String name;
 }
