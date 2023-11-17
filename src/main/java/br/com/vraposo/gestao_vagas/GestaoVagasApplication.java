9efe5d032a51166384b2557c09826046c878735b
vhraposo <thevhup@gmail.com>
2023-11-16 21:23:20 -0300

    feat: add Swagger permissions to SecurityConfig

--- src/main/java/br/com/vraposo/gestao_vagas/GestaoVagasApplication.java
+++ src/main/java/br/com/vraposo/gestao_vagas/GestaoVagasApplication.java
@@ -3,7 +3,17 @@ package br.com.vraposo.gestao_vagas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(
+	info = @Info(
+		title = "Gestão de Vagas",
+		description = "API para gestão de vagas de emprego",
+		version = "1"
+	)
+)
 public class GestaoVagasApplication {
 
 	public static void main(String[] args) {
